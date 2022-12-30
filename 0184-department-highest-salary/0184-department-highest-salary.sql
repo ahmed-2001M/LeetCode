31b30c8d2bd8532666a2ee39de43b699f4ab07a1
Ahmed Abdallah <67014587+ahmed-2001M@users.noreply.github.com>
2022-12-30 15:10:38 +0200

    Time: 772 ms (48.05%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -3,14 +3,14 @@ SELECT DISTINCT d.name as Department, e.name as Employee, e.salary
 FROM Employee as e
 INNER JOIN Department AS d
 ON e.departmentId = d.id
-,
+WHERE (e.salary , e.departmentId) IN
 (
 SELECT MAX(salary)  AS salary , departmentId
 FROM Employee
 GROUP BY departmentId
-) mx
+)
 
-where e.salary = mx.salary and e.departmentId = mx.departmentId
+# where e.salary = mx.salary and e.departmentId = mx.departmentId
 
 
 
