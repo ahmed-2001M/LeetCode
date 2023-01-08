4376e1231031ceea19ace6dda67aa1b6416bbf3b
Ahmed Abdallah <67014587+ahmed-2001M@users.noreply.github.com>
2023-01-08 19:05:21 +0200

    Time: 43 ms (99.68%), Space: 13.9 MB (58.17%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 : return False
+        
+        s = f'{x}'
+        l=0
+        r=len(s) -1
+        while l < r:
+            if s[l]!=s[r]:
+                return False
+            l+=1
+            r-=1
+        return True
+        
