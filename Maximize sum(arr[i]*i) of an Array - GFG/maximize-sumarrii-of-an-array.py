98a2626097bd98412b9181665a7525b3b6e2d771
Ahmed Abdallah <67014587+ahmed-2001M@users.noreply.github.com>
2023-06-21 18:02:48 +0300

    Added solution - LeetHub

---
+++ Maximize sum(arr[i]*i) of an Array - GFG/maximize-sumarrii-of-an-array.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    def Maximize(self, a, n): 
+        # Complete the function
+        a = sorted(a)
+        res = 0
+        for idx, val in enumerate(a):
+            res += int((val * idx) % (1e9+7))
+        return int(res % (1e9+7))
+      
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+for _ in range(0,int(input())):
+    n = int(input())
+    arr = list(map(int,input().strip().split()))
+    ob=Solution()
+    print(ob.Maximize(arr, n))
+    
+# } Driver Code Ends
